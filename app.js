3cafa374d79bf426e33171b42935f014db379c75
PedroMMIII <pedromonteirop12@gmail.com>
2024-10-30 15:06:19 -0300

    nav bar responsivo terminado

---
+++ app.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('.nav-toggle');
+const link = document.querySelector('.links');
+
+nav.addEventListener('click', function() {
+    link.classList.toggle('show-links');   
+})
